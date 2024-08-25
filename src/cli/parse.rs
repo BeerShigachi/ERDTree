8f001a665de20a4e07a1cedadbdf8677088041bc
Ayato Shiga-Callaway <beer.shigach@gmail.com>
2024-08-25 23:22:39 +0100

    implemented parsing. only accept single arg which is a path of dir.

--- src/cli/parse.rs
+++ src/cli/parse.rs
@@ -1,3 +1,15 @@
-pub fn parse_args() {
-    println!("Hello from parse.rs")
+use std::env;
+
+pub fn parse_args() -> Option<String> {
+    let args: Vec<String> = env::args().skip(1).collect();
+
+    match args.as_slice() {
+        [arg] => Some(arg.clone()),
+
+        _ => {
+            eprintln!("Error: Expected exactly one argument.");
+            eprintln!("Usage: erd-tree <your_directory>");
+            std::process::exit(1);
+        }
+    }
 }
